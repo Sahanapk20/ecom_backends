3bd1b618fb8860f8f708e01ae24a7a98dee28a0c
Sahanapk20 <pksahana2@gmail.com>
2025-03-19 16:45:31 +0530

    seconds

--- server.js
+++ server.js
@@ -6,20 +6,15 @@ const cartrouter = require('./routes/cartRoutes');
 
 
 const app=express();
-const allowedOrigins=["ecom-frontend-umber.vercel.app","ecom-frontend-git-main-sahana-p-k-s-projects.vercel.app",
-"ecom-frontend-gbivgka0h-sahana-p-k-s-projects.vercel.app" 
-]
+// const allowedOrigins=["ecom-frontend-umber.vercel.app","ecom-frontend-git-main-sahana-p-k-s-projects.vercel.app",
+// "ecom-frontend-gbivgka0h-sahana-p-k-s-projects.vercel.app" 
+// ]
+
+// 
 
 app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Allows cookies and authentication headers if needed
-  }))
+    origin: 'ecom-frontend-umber.vercel.app',
+  }));
 app.use(express.json())
 
 connectDB()
